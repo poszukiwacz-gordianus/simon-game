05f0af97c682e0bd725564d9a9338546f34dc5a4
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-11-21 14:00:07 +0100

    types: add new types

--- content/content.tsx
+++ content/content.tsx
@@ -1,6 +1,6 @@
-import { Rules } from "@/types/types";
+import { type RulesContent } from "@/types/types";
 
-export const rulesContent: Rules = [
+export const rulesContent: RulesContent = [
   "The game consists of multiple levels. The objective is to repeat the sequence of buttons played in each level.",
   'You are allowed to use up to 3 hints during each level. When you click the "Hint" button, the next button in the sequence will be shown.',
   "The sequence of buttons grows longer with each new level.",
--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
 import { Animated } from "react-native";
 
-export type Rules = Array<string>;
+export type RulesContent = Array<string>;
 
 export interface ButtonProps {
   title: string;
@@ -14,12 +15,12 @@ export interface GameFooterProps {
 
 export interface TileProps {
   color: string;
-  square: Animated.Value;
+  tile: Animated.Value;
   isPlaying: boolean;
 }
 
 export interface TilesContainerProps {
-  tiles: { color: string; square: Animated.Value }[];
+  tiles: { color: string; tile: Animated.Value }[];
   isPlaying: boolean;
 }
 
@@ -27,3 +28,89 @@ export interface RuleProps {
   rule: string;
   index: number;
 }
+
+// GameContext types
+/**
+ * The shape of the game state.
+ */
+
+export interface DifficultyState {
+  /**
+   * The current level for the difficulty.
+   */
+
+  level: number;
+}
+
+export interface GameState {
+  /**
+   * The game difficulty, or undefined if not set.
+   */
+  readonly difficulty: "easy" | "medium" | "hard";
+
+  /**
+   * The state for each difficulty.
+   */
+  readonly difficulties: {
+    easy: DifficultyState;
+    medium: DifficultyState;
+    hard: DifficultyState;
+  };
+
+  /**
+   * The number of levels to go.
+   */
+  togo: number;
+
+  /**
+   * The number of hints remaining.
+   */
+  hints: number;
+
+  /**
+   * Whether the game is currently playing.
+   */
+  isPlaying: boolean;
+}
+
+export interface GameContextType {
+  /**
+   * The current state of the game.
+   */
+  readonly state: GameState;
+
+  /**
+   * The function to dispatch an action to the reducer.
+   */
+  readonly dispatch: React.Dispatch<Action>;
+}
+
+/**
+ * Props for the GameProvider component.
+ */
+export interface GameContextProviderProps {
+  /**
+   * The children to be rendered inside the provider.
+   */
+  readonly children: ReactNode;
+}
+
+/**
+ * An action to set the game difficulty.
+ */
+export interface DifficultyAction {
+  /**
+   * The type of the action.
+   */
+  readonly type: "difficulty";
+
+  /**
+   * The difficulty to set.
+   */
+  readonly payload: "easy" | "medium" | "hard";
+}
+
+/**
+ * The type of all possible actions.
+ */
+export type Action = DifficultyAction;
