f4e2cbbf48ac89da7ddec43ad23e971e58bf20f1
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-11-21 15:18:20 +0100

    change: View to ScrollView

--- app/levels.tsx
+++ app/levels.tsx
@@ -1,6 +1,6 @@
 import { useGameContext } from "@/context/GameContext";
 import { Link } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function ShowLevels() {
   const {
@@ -12,32 +12,38 @@ export default function ShowLevels() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Choose level</Text>
-      <View style={styles.levelContainer}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          flexDirection: "row",
+          flexWrap: "wrap",
+          padding: 10,
+          paddingBottom: 80,
+          justifyContent: "center",
+        }}
+      >
         {Array.from({ length: 40 }, (_, index) => {
           const isBlocked = index + 1 > unblockedLevels;
 
           return (
-            <Pressable
+            <Link
+              key={index}
+              href={{ pathname: "/game", params: { level: index + 1 } }}
+              disabled={isBlocked}
               style={[
                 styles.level,
                 {
                   backgroundColor: isBlocked ? "#373837" : "#1a8412",
                 },
               ]}
-              key={index}
             >
-              <Link
-                href={{ pathname: "/game", params: { level: index + 1 } }}
-                key={index}
-                style={{ color: "#FEF2BF", textAlign: "center" }}
-                disabled={isBlocked}
-              >
+              <Text style={{ color: "#FEF2BF", textAlign: "center" }}>
                 {index + 1}
-              </Link>
-            </Pressable>
+              </Text>
+            </Link>
           );
         })}
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -46,6 +52,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#437214",
+    alignItems: "center",
+    justifyContent: "center",
   },
   header: {
     marginTop: 25,
@@ -53,20 +61,11 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: "center",
   },
-  levelContainer: {
-    marginTop: 20,
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    padding: 10,
-  },
   level: {
     width: "20%",
-    aspectRatio: 1,
-    justifyContent: "center",
-    textAlign: "center",
     padding: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
     margin: 5,
   },
 });
