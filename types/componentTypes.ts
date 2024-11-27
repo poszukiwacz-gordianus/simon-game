1e8cf956970ed092d3dc7521171df55e17eac347
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-11-27 15:15:33 +0100

    move: GenerateTIleSequenceProps from utilityTypes

--- types/componentTypes.ts
+++ types/componentTypes.ts
@@ -16,3 +16,17 @@ export interface RuleProps {
   rule: string;
   index: number;
 }
+
+/**
+ * Generates a sequence of tile indices for the current game level.
+ *
+ * @param length - A number representing the current level.
+ * @returns An array of numbers representing the sequence of tiles to guess by user.
+ */
+export type GenerateTileSequenceProps = (
+  level: number,
+  prevSequence: number[],
+  tiles: AnimatedTile[],
+  animationPace: number,
+  tileSound: () => void
+) => number[];
