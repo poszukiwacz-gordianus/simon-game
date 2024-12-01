f03ceb2d2458dc9d67acfb0f32334b7d80f1dd42
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-12-01 17:32:11 +0100

    update: remove useTriggerLevelStart add useHandleLevelUp

--- hooks/useHooks.ts
+++ hooks/useHooks.ts
@@ -1,17 +1,15 @@
-import useGenerateTileSequence from "./useGenerateTileSequence";
 import useHideAndroidNavigationBar from "./useHideAndroidNavigationBar";
 import useInitializeLevelSequence from "./useInitializeLevelSequence";
 import useLoadGameContent from "./useLoadGameContent";
 import useLoadOnAppStart from "./useLoadOnAppStart";
 import useLoadSoundsToMemory from "./useLoadSoundsToMemory";
-import useTriggerLevelStart from "./useTriggerLevelStart";
+import useHandleLevelUp from "./useHandleLevelUp";
 
 export {
   useLoadSoundsToMemory,
   useLoadGameContent,
-  useGenerateTileSequence,
   useHideAndroidNavigationBar,
   useInitializeLevelSequence,
   useLoadOnAppStart,
-  useTriggerLevelStart,
+  useHandleLevelUp,
 };
